60c19f61d1d14a7abac14cf4bc538674313f9a2c
Ewerton <ewerton.vasconcelos@poli.ufrj.br>
2024-06-24 15:14:34 +0200

    Create sql_formatter.py

---
+++ streamlit_apps/sql_formatter.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import sqlparse
+
+st.set_page_config(layout="wide")
+
+
+col1, col2 = st.columns([0.4,0.6], gap="small")
+
+in_code = col1.text_area("SQL Query", height=400)
+out_code = sqlparse.format(in_code, reindent=True, keyword_case='upper')
+col2.text('Formated Query')
+col2.code(out_code, language="sql", line_numbers=True)
