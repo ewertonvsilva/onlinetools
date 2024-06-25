ec30b0a3981c41fc85fabcfddeb5a57f6bf13344
ewertonvsilva <ewerton.vasconcelos@poli.ufrj.br>
2024-06-26 00:58:01 +0200

    adjusts.

--- start_local.sh
+++ start_local.sh
@@ -2,7 +2,8 @@ export FLASK_ENV=dev
 hap run -- flask run -h 0.0.0.0 -p 8081 
 hap run -- streamlit run streamlit_apps/list_to_array.py --server.port 8501
 hap run -- streamlit run streamlit_apps/code_editor.py --server.port 8502
-
+hap run -- streamlit run streamlit_apps/yaml_parser.py --server.port 8503
+hap run -- streamlit run streamlit_apps/sql_formatter.py --server.port 8504
 
 open http://0.0.0.0:8081
 hap status
--- stop_local.sh
+++ stop_local.sh
@@ -1,5 +1,7 @@
 hap kill 1
 hap kill 2
 hap kill 3
+hap kill 4
+hap kill 5
 hap clean
 hap status
