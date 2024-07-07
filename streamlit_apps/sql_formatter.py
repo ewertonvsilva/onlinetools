80ee839af5dca8d3aef5a11d9113b682ea6aa40d
ewertonvsilva <ewerton.vasconcelos@poli.ufrj.br>
2024-07-07 21:06:16 +0200

    Edit apps margin

--- streamlit_apps/sql_formatter.py
+++ streamlit_apps/sql_formatter.py
@@ -1,7 +1,26 @@
 import streamlit as st
 import sqlparse
 
-st.set_page_config(page_title='SQL Formater', layout="wide", page_icon=':material/join_inner:')
+st.set_page_config(
+    page_title="SQL Formater",
+    layout="wide",
+    page_icon=":material/join_inner:"
+)
+
+margins_css = """
+    <style>
+        .main > div {
+            padding-left: 1rem;
+            padding-right: 2rem;
+            padding-top: 0rem;
+        }
+        header {display: none!important;}
+    </style>
+"""
+
+st.markdown(margins_css, unsafe_allow_html=True)
+
+# st.set_page_config(page_title='SQL Formater', layout="wide", page_icon=':material/join_inner:')
 
 
 col1, col2 = st.columns([0.4,0.6], gap="small")
--- streamlit_apps/yaml_parser.py
+++ streamlit_apps/yaml_parser.py
@@ -1,7 +1,27 @@
 import yaml
 import json
 import streamlit as st
-st.set_page_config(page_title='Yaml Parser',layout="wide", page_icon =':material/integration_instructions:' )
+
+st.set_page_config(
+    page_title="Yaml Parser",
+    layout="wide",
+    page_icon=":material/integration_instructions:"
+)
+
+margins_css = """
+    <style>
+        .main > div {
+            padding-left: 1rem;
+            padding-right: 2rem;
+            padding-top: 0rem;
+        }
+        header {display: none!important;}
+    </style>
+"""
+
+st.markdown(margins_css, unsafe_allow_html=True)
+
+# st.set_page_config(page_title='Yaml Parser',layout="wide", page_icon =':material/integration_instructions:' )
 
 
 col1, col2 = st.columns([0.4,0.6], gap="small")
